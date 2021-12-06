f51b3aff4ddd3b57631d0c364f1ede5a2892f228
Joël Weber <joel@joelweber.nl>
2021-12-06 15:40:30 +0100

    Remove webscraper for verzekeraars
    This method was a bit sketchy, and relied on a legal gray area.

--- assets/js/scraper.js
+++ assets/js/scraper.js
@@ -1,28 +1,28 @@
-$(document).ready(function() {
-  $.ajax({
-    url: 'https://cors.seriumtech.workers.dev/?' + "https://www.zorgwijzer.nl/vergoeding/fysiotherapie",
-    dataType: 'text',
-    success: function(data) {
-      var el = $("<div>").html(data)
-      $('#table').replaceWith($('.verzekeraars', el));
-      $('.verzekeraars').children().attr('id', 'item');
-      $('#item.cell.box').remove();
-      $('form#item.feedback').remove();
-      $(".verzekeraars h3").each(function( index ) {
-        $(this).parent().remove();
-      });
-      $(".verzekeraars tr").each(function( index ) {
-        $(this).find('td,th').last().remove();
-      });
-    }
-  });
+// $(document).ready(function() {
+//   $.ajax({
+//     url: 'https://cors.seriumtech.workers.dev/?' + "https://www.zorgwijzer.nl/vergoeding/fysiotherapie",
+//     dataType: 'text',
+//     success: function(data) {
+//       var el = $("<div>").html(data)
+//       $('#table').replaceWith($('.verzekeraars', el));
+//       $('.verzekeraars').children().attr('id', 'item');
+//       $('#item.cell.box').remove();
+//       $('form#item.feedback').remove();
+//       $(".verzekeraars h3").each(function( index ) {
+//         $(this).parent().remove();
+//       });
+//       $(".verzekeraars tr").each(function( index ) {
+//         $(this).find('td,th').last().remove();
+//       });
+//     }
+//   });
   
-    $("#search").on("keyup", function() {
-      var value = $(this).val().toLowerCase().replace(/ /g, '-');
-      if (!value){
-        $('.verzekeraars').children().show();
-      } else {
-        $('.verzekeraars').children().not('[data-name*="'+ value +'"]').hide();
-      }
-    });
-  });
+//     $("#search").on("keyup", function() {
+//       var value = $(this).val().toLowerCase().replace(/ /g, '-');
+//       if (!value){
+//         $('.verzekeraars').children().show();
+//       } else {
+//         $('.verzekeraars').children().not('[data-name*="'+ value +'"]').hide();
+//       }
+//     });
+//   });
